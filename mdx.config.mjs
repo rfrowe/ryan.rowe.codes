0ba2b5e3eacfcb686f589b347fbb435e49537a8f
Ryan Rowe <ryanf.rowe@gmail.com>
2022-03-11 14:37:27 -0800

    Add MDX support

---
+++ mdx.config.mjs
@@ -0,0 +1,10 @@
+const MdxConfig = {
+    extension: /\.mdx?$/,
+    /** @type {import('@mdx-js/loader').Options} */
+    options: {
+        remarkPlugins: [],
+        rehypePlugins: [],
+    },
+}
+
+export default MdxConfig
