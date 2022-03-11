6a8c22b705f1ba9ce7f239b4c5cbfc80c0f2aa01
Ryan Rowe <ryanf.rowe@gmail.com>
2022-03-11 14:42:32 -0800

    tsconfig: add paths for shorter imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
-import '../styles/globals.css'
+import '@styles/globals.css'
 import type { AppProps } from 'next/app'
-import Tina from '../.tina/components/TinaDynamicProvider.js'
+import Tina from '@tina/components/TinaProvider'
 
 function App({ Component, pageProps }: AppProps) {
   return (
