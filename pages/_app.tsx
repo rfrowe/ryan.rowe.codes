5adb426c27b3a9d38bfe9861025b8eff719a31cb
Ryan Rowe <ryanf.rowe@gmail.com>
2022-07-20 16:25:41 -0700

    pages/_app: enable theming

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,19 @@
+import {ThemeModeProvider} from "@components/theming";
 import DynamicTina from "@components/tina/dynamic"
 import {CssBaseline} from "@mui/material";
 import '@styles/globals.css'
 
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
 
-function App({ Component, pageProps }: AppProps) {
-  return (
-      <DynamicTina>
-        <CssBaseline />
-        <Component {...pageProps} />
-      </DynamicTina>
-  )
+function App({Component, pageProps}: AppProps) {
+    return (
+        <DynamicTina>
+            <ThemeModeProvider>
+                <CssBaseline/>
+                <Component {...pageProps} />
+            </ThemeModeProvider>
+        </DynamicTina>
+    )
 }
 
 export default App
