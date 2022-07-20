23ab6236744cc31bf62ffc416ef014421ce336ad
Ryan Rowe <ryanf.rowe@gmail.com>
2022-07-20 15:15:25 -0700

    pages/_app: add baseline MUI css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import DynamicTina from "@components/tina/dynamic"
+import {CssBaseline} from "@mui/material";
 import '@styles/globals.css'
 
 import type { AppProps } from 'next/app'
@@ -6,6 +7,7 @@ import type { AppProps } from 'next/app'
 function App({ Component, pageProps }: AppProps) {
   return (
       <DynamicTina>
+        <CssBaseline />
         <Component {...pageProps} />
       </DynamicTina>
   )
