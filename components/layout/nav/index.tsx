4ccea38a2d91cedce8e1ce03a1f8a56cea267476
Ryan Rowe <ryanf.rowe@gmail.com>
2022-08-02 11:34:58 -0700

    layout/nav: animate root link for email

--- components/layout/nav/index.tsx
+++ components/layout/nav/index.tsx
@@ -1,4 +1,5 @@
 import NavBar from "@components/layout/nav/bar";
+import Namemail from "@components/layout/nav/namemail"
 import NavBarIconLink from "@components/layout/nav/icon";
 import NavBarLink from "@components/layout/nav/link";
 import NavBarThemeSwitcher from "@components/layout/nav/theme";
@@ -9,6 +10,7 @@ import HomeIcon from '@mui/icons-material/Home';
 import {Box, Theme} from "@mui/material";
 
 import {useRouter} from "next/router";
+import {useState} from "react";
 
 const hideOnLarge = (hide: boolean) => (theme: Theme) => css({
     display: hide ? 'none' : 'unset',
@@ -28,16 +30,23 @@ const Navigation = () => {
     const route = useRouter()
     const isRoot = route.pathname === '/'
 
+    const [showEmail, setShowEmail] = useState(false)
+
     return (
         <NavBar>
             <NavBarThemeSwitcher />
             <NavBarIconLink href='https://github.com/rfrowe' icon={GitHubIcon} />
-            <NavBarIconLink href='mailto:ryan@rowe.codes' icon={EmailIcon} />
+            <NavBarIconLink
+                href='mailto:ryan@rowe.codes'
+                icon={EmailIcon}
+                onMouseEnter={() => setShowEmail(true)}
+                onMouseLeave={() => setShowEmail(false)}
+            />
 
             {/* Root links, which are only shown on non-root pages, for mobile and desktop. */}
             <NavBarIconLink href='/' icon={HomeIcon} css={hideOnLarge(isRoot)} />
             <NavBarLink href='/' css={hideOnSmall(isRoot)}>
-                ryan.rowe.codes
+                <Namemail showEmail={showEmail} />
             </NavBarLink>
 
             <Box css={css({marginLeft: 'auto'})}>
